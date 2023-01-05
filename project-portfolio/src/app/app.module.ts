63a5465cee07072533927cc63492d04a8ffcea9a
Raghav Bansal <raghav12bansal@gmail.com>
2023-01-05 09:03:11 +0530

    adding bare-bone structure of Header, Home and Projects Section

--- project-portfolio/src/app/app.module.ts
+++ project-portfolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
